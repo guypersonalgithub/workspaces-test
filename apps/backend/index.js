8bf742ae35ecda13046a494f6e75be654900db7e
guypersonalgithub <zeraptos@gmail.com>
2023-04-08 02:40:42 +0300

    ...

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -5,8 +5,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-console.log("??");
-
 app.listen(process.env.PORT || 3001, () => {
   console.log(`Server is running on port 3001`);
 });
